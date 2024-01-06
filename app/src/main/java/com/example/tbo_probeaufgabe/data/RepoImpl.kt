09407a04611266bdc824413c57c60577a2584218
nurseiit.tursunkulov <nurseiit.tursunkulov@check24.de>
2024-01-06 19:09:35 +0100

    added basic logic to repo

--- app/src/main/java/com/example/tbo_probeaufgabe/data/RepoImpl.kt
+++ app/src/main/java/com/example/tbo_probeaufgabe/data/RepoImpl.kt
@@ -5,6 +5,8 @@ import com.example.tbo_probeaufgabe.data.remote.RemoteDataSource
 import com.example.tbo_probeaufgabe.data.remote.model.CoinHistoryLocalModel.Companion.toDomainModel
 import com.example.tbo_probeaufgabe.domain.model.Coin
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.map
 
 
@@ -15,15 +17,32 @@ import kotlinx.coroutines.flow.map
 class RepoImpl(val localDataSource: LocalDataSource, val remoteDataSource: RemoteDataSource) :
     Repository {
     override suspend fun getCoins(): Flow<List<Coin>> {
-        return localDataSource.getCoins().map {
-
-            it.map {
-                it.toDomainModel().apply {
-                    historyPrice = localDataSource.getCoinHistory(it.id)?.let {
+        val coinsFromCahs = localDataSource.getCoins().map {coinApiModelList->
+            coinApiModelList.map {coinApiModelList->
+                coinApiModelList.toDomainModel().apply {
+                    historyPrice = localDataSource.getCoinHistory(coinApiModelList.id)?.let {
                         toDomainModel(it)
                     }
                 }
             }
         }
+        if (coinsFromCahs.isEmpty()) {
+            val coinsFromNetwork = remoteDataSource.getCoins()
+            localDataSource.insertCoins(coinsFromNetwork)
+            coinsFromNetwork.map {coinApiModel->
+                coinApiModel.apply {
+                    remoteDataSource.getCoinHistory(coinApiModel.id).let {coinHistory->
+                        localDataSource.insertCoinHistory(coinHistory.toLocalModel(coinApiModel.id))
+                    }
+                }
+            }
+        }
+        return coinsFromCahs
     }
+}
+
+suspend fun <T> Flow<T>.isEmpty(): Boolean{
+    this.firstOrNull()?.let {
+        return false
+    }?: return true
 }
